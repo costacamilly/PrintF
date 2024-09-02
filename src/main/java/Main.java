d9f882b025721a82662868005e406d81a54154c1
Camilly <169068284+costacamilly@users.noreply.github.com>
2024-09-02 17:11:49 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+  public static void main(String[] args) {
+String nome = "camilly";
+int dia = 17;
+int mes = 12;
+int ano = 2009;
+double pi = 3.14159; 
+System.out.printf("o valor de pi é: %.2f" ,pi);
+  
+// System.out.prinf("eu nasci no dia %d do mes %d do ano %d" dia, mes, ano);
+ 
+  }
+
+  
+}
